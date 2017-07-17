3120f870af9df7d4e4b2e104ea022b7f48686aab
danweizhao <danweizhao@edayun.cn>
2017-07-17 15:21:01 +0800

    hello

---
+++ day1/var.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+print ("hello world")
+name = "111"
+name2=name
+print ("ssss",name,name2)
+name = "4444"
+name2=name
+print (name,name2)
+gf_of_oldboy="2222"
+
